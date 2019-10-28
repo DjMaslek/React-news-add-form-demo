e838f271235f98cbc037ccc020f1a0c9d595bc7e
DJMaslek <realmaslek@gmail.com>
2019-10-28 16:36:44 +0700

    Updates

---
+++ precache-manifest.8961a01df40cc0d9303669c65fb2f137.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6032a0baf18cd882687f9dd0092e62",
+    "url": "/React-news-add-form-demo/index.html"
+  },
+  {
+    "revision": "4b7cd90d32d9bf055716",
+    "url": "/React-news-add-form-demo/static/css/main.c7b9f6cb.chunk.css"
+  },
+  {
+    "revision": "51e73b75f3a11f65cf4c",
+    "url": "/React-news-add-form-demo/static/js/2.bc9e1127.chunk.js"
+  },
+  {
+    "revision": "4b7cd90d32d9bf055716",
+    "url": "/React-news-add-form-demo/static/js/main.da9532e4.chunk.js"
+  },
+  {
+    "revision": "c70de2f06eaeeba6b8c2",
+    "url": "/React-news-add-form-demo/static/js/runtime-main.f9505bc2.js"
+  }
+]);
